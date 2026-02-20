364d5beb7b814292e34d1d15974bbda5806e70e2
Salah Haddani <salahabdelwahabe@example.com>
2026-02-20 13:54:28 +0000

    changes

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ myIo(io);
 
 console.log(`Server listening on port ${config.port}`);
 
-const Handlebars = handlebars.create({
+/*const Handlebars = handlebars.create({
   extname: ".html",
   partialsDir: path.join(__dirname, "..", "front", "views", "partials"),
   defaultLayout: false,
@@ -38,6 +38,6 @@ app.set("views", path.join(__dirname, "..", "front", "views"));
 app.use(
   "/public",
   express.static(path.join(__dirname, "..", "front", "public"))
-);
+);*/
 
 routes(app);
