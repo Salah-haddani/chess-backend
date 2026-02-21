7e76a8cbc590d3136b44f441a2b29965e9c769c3
Salah Haddani <salahabdelwahabe@example.com>
2026-02-21 15:40:31 +0000

    chat changes

--- server/sockets/io.js
+++ server/sockets/io.js
@@ -1,37 +1,40 @@
+module.exports = (io) => {
+  io.on("connection", (socket) => {
+    console.log("New socket connection");
 
+    let currentCode = null;
 
-module.exports = io => {
-    io.on('connection', socket => {
-        console.log('New socket connection');
+    socket.on("move", function (move) {
+      console.log("move detected");
 
-        let currentCode = null;
-
-        socket.on('move', function(move) {
-            console.log('move detected')
-
-            io.to(currentCode).emit('newMove', move);
-        });
-        
-        socket.on('joinGame', function(data) {
-
-            currentCode = data.code;
-            socket.join(currentCode);
-            if (!games[currentCode]) {
-                games[currentCode] = true;
-                return;
-            }
-            
-            io.to(currentCode).emit('startGame');
-        });
-
-        socket.on('disconnect', function() {
-            console.log('socket disconnected');
-
-            if (currentCode) {
-                io.to(currentCode).emit('gameOverDisconnect');
-                delete games[currentCode];
-            }
-        });
-
+      io.to(currentCode).emit("newMove", move);
     });
+
+    socket.on("joinGame", function (data) {
+      currentCode = data.code;
+      socket.join(currentCode);
+      if (!games[currentCode]) {
+        games[currentCode] = true;
+        return;
+      }
+
+      io.to(currentCode).emit("startGame");
+    });
+
+    socket.on("disconnect", function () {
+      console.log("socket disconnected");
+
+      if (currentCode) {
+        io.to(currentCode).emit("gameOverDisconnect");
+        delete games[currentCode];
+      }
+    });
+    socket.on("chatMessage", function (data) {
+      // Broadcast to the specific game room
+      io.to(data.code).emit("newMessage", {
+        user: data.user,
+        message: data.message,
+      });
+    });
+  });
 };
